53834f18dd1e09d74ff0f235cbfc558616b4b4f4
icewassim <wassimbenameur@gmail.com>
2017-05-21 01:33:33 +0200

    add protection & search params, fix empty zipcodelist query

--- server/api/articles/article.controller.js
+++ server/api/articles/article.controller.js
@@ -42,10 +42,60 @@ class ArticlesController {
     });
   }
 
-  getArticlePostParams(req, res, next) {
-    articlesCollection.findOne(req.params, (err, doc) => {
+  getArticlesPostParams(req, res, next) {
+    let typeLocation = [];
+    let zipCodesQuery = {};
+    let query;
+
+    if(!req.params) {
+      res.status(500).json({error:'bad request missing params'});
+      return;
+    }
+
+    let isPro = req.params.isPro + 0 ;
+    let isPerso = req.params.isPerso + 0 ;
+
+    if(req.params.isStudio) {
+      typeLocation.push(0);
+    }
+    if(req.params.isMaison) {
+      typeLocation.push(1);
+    }
+    if(req.params.isAppart) {
+      typeLocation.push(2);
+    }
+    if(req.params.isGarage) {
+      typeLocation.push(3);
+    }
+
+    query = {
+        '$or':[{'isPerso': isPerso},{'isPro': isPro}],
+        'type' : {'$in' : typeLocation},
+      }
+    ;
+
+    if(req.params.zipCodes && req.params.zipCodes.length > 0) {
+      let zipCodesList = req.params.zipCodes;
+      query.zipCode = {'$in' : zipCodesList};
+    }
+
+    if(req.params.price) {
+      let priceQuery = {};
+      if(req.params.price.lower) {
+        priceQuery['$gt'] = req.params.price.lower;
+      }
+      if(req.params.price.upper) {
+        priceQuery['$lt'] = req.params.price.upper;
+      }
+      query.price = priceQuery;
+    }
+
+    console.log('zipCodes ', zipCodesQuery);
+    console.log('typeLocation', typeLocation);
+
+    articlesCollection.find(query, (err, doc) => {
       if(err) {
-        return next(new errors.InvalidContentError(err.errors.name.message));
+        return next(new errors.InvalidContentError(err));
       }
       res.send(doc);
       next();
--- server/api/articles/index.js
+++ server/api/articles/index.js
@@ -5,5 +5,5 @@ const articlesController = require('./article.controller');
 module.exports = function(server) {
   server.get('/articles', articlesController.getArticles);
   server.get('/article/:article_id', articlesController.getArticleById);
-  server.post('/articles-post', articlesController.getArticlePostParams);
+  server.post('/articles-post', articlesController.getArticlesPostParams);
 }
